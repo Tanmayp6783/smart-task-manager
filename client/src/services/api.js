7ec4a9d96cbe97ccfaf8afa4c1439eb278e61eeb
Tanmay Patil <tanmayp205@gmail.com>
2026-07-27 15:32:20 +0530

    Configure frontend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
 });
 
-// Automatically attach JWT token to every request
+// Automatically attach JWT token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
